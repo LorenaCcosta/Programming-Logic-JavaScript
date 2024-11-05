da7966fc49d8a7d25c6112377351093badc5b346
Lorena Araujo Costa <lorenaaraujo954@gmail.com>
2024-11-04 22:22:48 -0300

    Update atividade3.js

--- SWITCH/atividade3.js
+++ SWITCH/atividade3.js
@@ -5,26 +5,18 @@ const escolherPlanos = readlineSync.questionInt("Qual plano de telefonia voce de
 let preco;
 switch (escolherPlanos) {
     case 1: 
-    preco = 44.99;
-        console.log("Com o plano Básico você pode fazer até 2 ligações internacionais por mês, 10GB de internet para usar onde quiser e mais 2GB de pacote Bônus de internet pagando sua fatura todo mês no pix!");
+        console.log("Com o plano Básico você pode fazer até 2 ligações internacionais por mês, 10GB de internet para usar onde quiser e mais 2GB de pacote Bônus de internet pagando sua fatura todo mês no pix! Por apenas 44,99!");
     break;
 
-    case 2:  
-    preco = 74.99;
-        console.log("Com o plano Intermediário, você tem ligações ilimitadas para fora do país para ligar para onde quiser, 32GB de internet e whatsApp ilimitado! ");
+    case 2:
+        console.log("Com o plano Intermediário, você tem ligações ilimitadas para fora do país para ligar para onde quiser, 32GB de internet e whatsApp ilimitado! Por apenas 74,99!");
     break;
 
-    case 3: 
-    preco = 109.99;
-        console.log("Com o plano Premium, você tem 56GB de internet, ligações ilimitadas para fora e dentro do país, incluso a proteção do seu smartphone durante 12 meses, além de concorrer a um smartphone Samsung Galaxy S24 Ultra todo mês pagando no cartão de crédito!");
+    case 3:
+        console.log("Com o plano Premium, você tem 56GB de internet, ligações ilimitadas para fora e dentro do país, incluso a proteção do seu smartphone durante 12 meses, além de concorrer a um smartphone Samsung Galaxy S24 Ultra todo mês pagando no cartão de crédito! Por apenas 109,99!");
     break;
 
     default:
         console.log("Resposta inválida. Escolha 1, 2 ou 3");
         preco = 0;
     break;
-}
-    if (preco > 0) {
-        console.log(`O preço do serviço escolhido é R$ ${preco.toFixed(2
-        )}.`);
-        }
