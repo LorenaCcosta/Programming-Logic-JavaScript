ba0d9ae5cb75d7456502811f7db18d4aaebb0fdf
LorenaCcosta <lorenaaraujo954@gmail.com>
2024-11-23 16:38:44 -0300

    update

--- WHILE/atividade5.js
+++ WHILE/atividade5.js
@@ -1,1 +1,16 @@
 const readlineSync = require('readline-sync');
+
+const maxAlunos = 30;
+let contadorAlunos = 0;
+
+while (contadorAlunos < maxAlunos) {
+    const nomeAluno = readlineSync.question("Digite o nome do aluno: ")
+    const nota = readlineSync.question(`Digite a nota do(a) aluno(a) ${nomeAluno}: `)
+
+    console.log(`O(a) aluno(a) ${nomeAluno} tem a nota: ${nota}`);
+
+    contadorAlunos++;
+    
+}
+
+ console.log(`Todos os ${contadorAlunos} alunos já foram inseridos as notas!`);
